c4b94f5a46ea257eb387ecaf43e01edccd3e83e1
Filipe Silva <filipe.silva@hammerconsult.com.br>
2020-05-07 20:52:55 -0300

    Testando Categoria domain

---
+++ src/main/java/com/apploja/resource/CategoriaResource.java
@@ -0,0 +1,29 @@
+package com.apploja.resource;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.apploja.domain.Categoria;
+
+@RestController
+@RequestMapping(value="/categorias")
+public class CategoriaResource {
+
+	@GetMapping
+	public List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1,"Informática");
+		Categoria cat2 = new Categoria(2,"Escritorio");
+		
+		List <Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
+	}
+	
+}
