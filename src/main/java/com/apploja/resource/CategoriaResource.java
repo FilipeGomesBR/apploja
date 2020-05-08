90961200b7b07c160704908ebd74214e96526018
Filipe Silva <filipe.silva@hammerconsult.com.br>
2020-05-08 18:37:24 -0300

    Produtos e associação Muito para Muitos

--- src/main/java/com/apploja/resource/CategoriaResource.java
+++ src/main/java/com/apploja/resource/CategoriaResource.java
@@ -3,27 +3,29 @@ package com.apploja.resource;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.apploja.domain.Categoria;
+import com.apploja.services.CategoriaService;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 
-	@GetMapping
-	public List<Categoria> listar() {
+	@Autowired
+	private CategoriaService service;
+	
+	@GetMapping(value="/{id}")
+	public ResponseEntity<?> find(@PathVariable Integer id) {
 		
-		Categoria cat1 = new Categoria(1,"Informática");
-		Categoria cat2 = new Categoria(2,"Escritorio");
-		
-		List <Categoria> lista = new ArrayList<>();
-		lista.add(cat1);
-		lista.add(cat2);
-		
-		return lista;
+		Categoria obj = service.buscar(id);
+		return ResponseEntity.ok().body(obj);
+			
 	}
 	
 }
